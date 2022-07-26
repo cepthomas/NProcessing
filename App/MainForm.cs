9ae132683461f816d9e302299cd2d0cdd218a5b9
cepthomas <cepthomas@gmail.com>
2022-07-26 17:30:38 -0500

    WIP.

--- App/MainForm.cs
+++ App/MainForm.cs
@@ -89,8 +89,8 @@ namespace NProcessing.App
 
             // The rest of the controls.
             textViewer.BackColor = _settings.BackColor;
-            textViewer.Colors.Add("ERR", Color.LightPink);
-            textViewer.Colors.Add("WRN", Color.Plum);
+            textViewer.MatchColors.Add("ERR", Color.LightPink);
+            textViewer.MatchColors.Add("WRN", Color.Plum);
             textViewer.Prompt = "> ";
 
             ///// CPU meter /////
