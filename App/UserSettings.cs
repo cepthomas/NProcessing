066a1f24137465955a8e6a2e90bfbbe590b478a5
cepthomas <cepthomas@gmail.com>
2022-08-24 12:42:13 -0500

    WIP.

--- App/UserSettings.cs
+++ App/UserSettings.cs
@@ -18,7 +18,7 @@ using NBagOfUis;
 namespace NProcessing.App
 {
     [Serializable]
-    public class UserSettings : Settings
+    public sealed class UserSettings : Settings
     {
         #region Persisted editable properties
         // [DisplayName("Script Path")]
